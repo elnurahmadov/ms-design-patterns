07401a3e946270b8a30058b1b0395d94c8b6bd4c
elnur.ahmadov <elnur.ahmadov@cybernet.az>
2026-01-03 11:59:51 +0400

    Prototype Pattern

---
+++ src/main/java/az/ingress/prototype/ProjectFactory.java
@@ -0,0 +1,13 @@
+package az.ingress.prototype;
+
+public class ProjectFactory {
+    private final Project project;
+
+    public ProjectFactory(Project project) {
+        this.project = project;
+    }
+
+    public Project cloneProject() {
+        return (Project) project.copy();
+    }
+}
---
+++ src/main/java/az/ingress/prototype/VersionControlCommands.java
@@ -0,0 +1,5 @@
+package az.ingress.prototype;
+
+public interface VersionControlCommands {
+    Object copy();
+}
